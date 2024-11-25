5adddf6ff85848a3e99dc1aaa2de22ca262c5aa7
Murray-vm <murray@locallizard.co.za>
2024-11-25 16:20:48 +0200

    update redis 4

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,7 @@
 # REDIS = Redis.new(url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1"))
 require 'redis'
 
-Redis.current = Redis.new(
+REDIS_CLIENT = Redis.new(
   url: ENV['REDIS_URL'],
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # Disable strict SSL verification
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
 )
-
