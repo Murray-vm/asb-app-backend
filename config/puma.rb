ea063323ebb501965c0e6d4b1783947b4b04e04a
Murray-vm <murray@locallizard.co.za>
2025-09-16 22:06:08 +0200

    reduce concuurency

--- config/puma.rb
+++ config/puma.rb
@@ -20,9 +20,9 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-workers ENV.fetch("WEB_CONCURRENCY", 2) if ENV["RAILS_ENV"] == "production"
+workers ENV.fetch("WEB_CONCURRENCY", 1) if ENV["RAILS_ENV"] == "production"
 
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 2)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
